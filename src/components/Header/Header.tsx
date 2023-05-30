228eaff7e509a01eed4feb6ad6cf87693fd9bd44
diego callegari <callcallegari@gmail.com>
2023-05-30 19:53:03 +0200

    Create NavigationStyled

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import HeaderStyled from "./HeaderStyled";
 
 const Header = (): React.ReactElement => {
   return (
-    <HeaderStyled>
+    <HeaderStyled className="header-component">
       <h1 className="header-title">QUÈ FEM</h1>
     </HeaderStyled>
   );
---
+++ src/components/Navigation/NavigationStyled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const NavigationStyled = styled.nav`
+  width: 320px;
+  height: 65px;
+  background-color: ${(props) => props.theme.colors.dark};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 25px 0px 25px;
+  width: 100%;
+  min-width: 320px;
+  position: fixed;
+  bottom: 0;
+
+  .navigation-icono {
+    color: ${(props) => props.theme.colors.clean};
+    color: ${(props) => props.theme.colors.confirm};
+  }
+`;
+
+export default NavigationStyled;
