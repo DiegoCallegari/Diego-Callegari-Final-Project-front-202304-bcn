17c9f9d7b03f8b6ba84057a599ace667229bcc3c
diego callegari <callcallegari@gmail.com>
2023-05-30 14:33:26 +0200

    Add Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import App from "../App/App";
+import LayoutStyled from "./LayoutStyled";
+
+const Layout = (): JSX.Element => {
+  return (
+    <LayoutStyled>
+      <App />
+    </LayoutStyled>
+  );
+};
+
+export default Layout;
