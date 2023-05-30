cf31d8eab63b73fbc70fff874a0ab8a893b2a508
diego callegari <callcallegari@gmail.com>
2023-05-30 17:54:55 +0200

    Add Layout component test

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,20 @@ import "@fontsource/lato";
 import "@fontsource/metrophobic";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { RouterProvider } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle/GlobalStyle.js";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme/theme";
+import appRouter from "./routers/appRouter.js";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Provider store={store}>
-        <App />
+        <RouterProvider router={appRouter} />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
