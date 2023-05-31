9e8ff921b045edf27c70f3be3ca17d7c242387df
diego callegari <callcallegari@gmail.com>
2023-05-31 12:46:28 +0200

    Add userReducer test

---
+++ src/store/user/userReducer.test.tsx
@@ -0,0 +1,23 @@
+import {
+  InitialStateUserMock,
+  LoggedStateUserMock,
+  userTokenStructureMock,
+} from "../../mock/userMocks";
+import { loginUserActionCreator, userReducer } from "./userSlice";
+
+describe("Given a userReducer", () => {
+  describe("When it receives a current state 'false' and a loginUser action with an user as data", () => {
+    test("Then it should return the actualized current state with the user logged", () => {
+      const currentUserState = InitialStateUserMock;
+
+      const expectNewUserState = LoggedStateUserMock;
+
+      const newUserState = userReducer(
+        currentUserState,
+        loginUserActionCreator(userTokenStructureMock)
+      );
+
+      expect(newUserState).toStrictEqual(expectNewUserState);
+    });
+  });
+});
