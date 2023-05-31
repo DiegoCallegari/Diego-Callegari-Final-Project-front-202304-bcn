ed92a6d226c4d30a0493db8dc3bbcc1774b01049
diego callegari <callcallegari@gmail.com>
2023-05-31 21:15:55 +0200

    Add LoginPage test

--- src/pages/LoginPage/LoginPageStyled.ts
+++ src/pages/LoginPage/LoginPageStyled.ts
@@ -1,19 +1,21 @@
 import styled from "styled-components";
 
-const LoginPageStyled = styled.div`
+const LoginPageStyled = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
 
   .page-title {
+    width: 100%;
     min-width: 320px;
     height: 30px;
+    top: 90px;
+    border-bottom: 1px solid ${(props) => props.theme.colors.redLight};
     background-color: ${(props) => props.theme.colors.clean};
     color: ${(props) => props.theme.colors.softDark};
     font-size: ${(props) => props.theme.fontSizes.small};
     font-family: ${(props) => props.theme.fonts.primary};
     position: fixed;
-    top: 80px;
     display: flex;
     flex-direction: column;
     align-items: center;
