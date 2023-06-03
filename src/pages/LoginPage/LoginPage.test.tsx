74720a91603e9d996b6cfd1af6e94d7600b2f29e
diego callegari <callcallegari@gmail.com>
2023-06-03 14:19:07 +0200

    Add integration test for LoginPage

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,6 +1,13 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import LoginPage from "./LoginPage";
+import paths from "../../routers/paths";
 
 describe("Given a LoginPage", () => {
   describe("When it is rendered", () => {
@@ -14,6 +21,42 @@ describe("Given a LoginPage", () => {
         level: 2,
       });
       expect(pageTitle).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Given a LoginPage component", () => {
+  describe("When the user clicks the 'Login' button", () => {
+    test("Then it should log in the user and redirects him to the '/' path", async () => {
+      const usernameText = "Username:";
+      const passwordText = "Password:";
+      const usernameInputText = "Username";
+      const passwordInputText = "Password";
+      const buttonText = "Login";
+
+      const route: RouteObject[] = [
+        {
+          path: "/",
+          element: <LoginPage />,
+        },
+        {
+          path: "/home",
+        },
+      ];
+
+      const router = createMemoryRouter(route);
+
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const usernameTextField = screen.getByLabelText(usernameText);
+      const passwordTextField = screen.getByLabelText(passwordText);
+      const loginButton = screen.getByRole("button", { name: buttonText });
+
+      await userEvent.type(usernameTextField, usernameInputText);
+      await userEvent.type(passwordTextField, passwordInputText);
+      await userEvent.click(loginButton);
+
+      expect(router.state.location.pathname).toStrictEqual(paths.home);
     });
   });
 });
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -6,6 +6,7 @@ import { loginUserActionCreator } from "../../store/user/userSlice";
 import useUser from "../../hooks/useUser/useUser";
 import { useNavigate } from "react-router-dom";
 import useToken from "../../hooks/useToken/useToken";
+import paths from "../../routers/paths";
 
 const LoginPage = (): React.ReactElement => {
   const { getUserToken } = useUser();
@@ -19,7 +20,7 @@ const LoginPage = (): React.ReactElement => {
     if (token) {
       const userLoginData = await getTokenData(token);
       dispatch(loginUserActionCreator(userLoginData));
-      navigate("/home", { replace: true });
+      navigate(`${paths.home}`, { replace: true });
     }
   };
   return (
