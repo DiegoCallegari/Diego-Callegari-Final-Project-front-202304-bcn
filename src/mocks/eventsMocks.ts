3bb4c95d0c95b91d0f9e207b7e4ed87709ca10ed
diego callegari <callcallegari@gmail.com>
2023-06-06 12:21:45 +0200

    Add useEvents test

---
+++ src/mocks/eventsMocks.ts
@@ -0,0 +1,24 @@
+import { EventDataStructure } from "../store/events/types";
+
+export const eventsMocks: EventDataStructure[] = [
+  {
+    title: "Concert",
+    image:
+      "https://cdn.discordapp.com/attachments/1096796306695008286/1115235166374350878/concierto.jpeg",
+    neighbourhood: "El Born",
+    description: "",
+    date: "21/02/2023",
+    category: "apreciate",
+    id: "1234",
+  },
+  {
+    title: "Bunkers Sunset",
+    image:
+      "https://cdn.discordapp.com/attachments/1096796306695008286/1115298838467596288/bunkers.jpg",
+    neighbourhood: "El Carmel",
+    description: "",
+    date: "21/02/2023",
+    category: "nature",
+    id: "5678",
+  },
+];
