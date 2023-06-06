b33b76c492df2d3dd696869116dd3fe45a7ce3a0
diego callegari <callcallegari@gmail.com>
2023-06-06 21:35:30 +0200

    Add getEvent test for error case

--- src/hooks/useEvents/useEvents.test.tsx
+++ src/hooks/useEvents/useEvents.test.tsx
@@ -2,6 +2,8 @@ import { renderHook } from "@testing-library/react";
 import useEvents from "./useEvents";
 import { wrapper } from "../../utils/testUtils";
 import { eventsMocks } from "../../mocks/eventsMocks";
+import { server } from "../../mocks/server";
+import { errorHandlers } from "../../mocks/handlers";
 
 describe("Given a useEvents custom hook", () => {
   describe("When is called with the getEvents function", () => {
@@ -16,6 +18,24 @@ describe("Given a useEvents custom hook", () => {
       const events = await getEvents();
 
       expect(events).toStrictEqual(expectedEvents);
+    });
+
+    describe("When it calls with the getEvents function but it's not possible to connect to Api Rest", () => {
+      test("Then it should return an error message 'Can't get the list of events at this moment'", async () => {
+        server.resetHandlers(...errorHandlers);
+
+        const expectedError = "Can't get the list of events";
+
+        const {
+          result: {
+            current: { getEvents },
+          },
+        } = renderHook(() => useEvents(), { wrapper: wrapper });
+
+        const events = getEvents();
+
+        expect(events).rejects.toThrowError(expectedError);
+      });
     });
   });
 });
