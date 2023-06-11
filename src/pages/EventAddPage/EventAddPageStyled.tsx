ff24f480e6507d2884c1db206ca86902578cc610
diego callegari <callcallegari@gmail.com>
2023-06-11 16:18:07 +0200

    Create EventAddForm

---
+++ src/pages/EventAddPage/EventAddPageStyled.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const EventAddPageStyed = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-width: 320px;
+  gap: 20px;
+  padding-bottom: 80px;
+
+  .page-title {
+    width: 100%;
+    height: 40px;
+    border-bottom: 1px solid ${(props) => props.theme.colors.redLight};
+    background-color: ${(props) => props.theme.colors.clean};
+    color: ${(props) => props.theme.colors.softDark};
+    font-size: ${(props) => props.theme.fontSizes.small};
+    font-family: ${(props) => props.theme.fonts.primary};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    letter-spacing: 1.5px;
+  }
+`;
+
+export default EventAddPageStyed;
